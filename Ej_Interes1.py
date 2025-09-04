109d1d1b4e95284d2e66b6d1b68fa01eec70bf23
Santino Godoy <santinogodoygaldeano@gmail.com>
2025-09-03 21:19:16 -0300

    .

--- Ej_Interes1.py
+++
@@ -1,9 +0,0 @@
-monto_prestamo = float(input("Ingrese el monto del prestamo: "))
-cant_meses = int(input("Ingrese la cantidad de meses a pagar: "))
-INTERES_FIJO = 2 / 100
-
-monto_total = monto_prestamo * (1 + (INTERES_FIJO))**cant_meses
-
-cuota_mensual = (monto_prestamo * INTERES_FIJO) / (1 - (1 + INTERES_FIJO)**(-cant_meses))
-print(f"El monto total a devolver es de ${round(monto_total, 3)}")
-print(f"El valor de cada cuota mensual es de: ${round(cuota_mensual, 2)}")
